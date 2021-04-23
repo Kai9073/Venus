d6b14739194c41c22b6d2261a76adedb544c560e
geraldlol10 <geraldadvincula10@gmail.com>
2021-04-23 20:10:45 +0800

    remove require structure part

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
 require('dotenv').config();
-require('module-alias/register');
-
-require('./structure/Guild');
-require('./structure/Message');
 
 import Client from './base/Client';
 const client = new Client();
